6d62990074218d3ffc659ca16a42c330fce37c5f
Marat Mutalipov <maratios41@gmail.com>
2022-12-22 22:43:43 +0300

    Refactoring app. Improved auth service. Improved watchlist service. Improved user service

--- src/watchlist/watchlist.service.ts
+++ src/watchlist/watchlist.service.ts
@@ -1,28 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
-import { Watchlist } from './models/watchlist.model';
-import { CreateAssetResponse } from './response';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/sequelize'
+import { Watchlist } from './models/watchlist.model'
+import { CreateAssetResponse } from './response'
 
 @Injectable()
 export class WatchlistService {
-    constructor(@InjectModel(Watchlist) private readonly watchlistRepository: typeof Watchlist) {}
+	constructor(
+		@InjectModel(Watchlist)
+		private readonly watchlistRepository: typeof Watchlist
+	) {}
 
-    async createAsset (user, dto): Promise<CreateAssetResponse> {
-        const watchlist = {
-            user: user.id,
-            name: dto.name,
-            assetId: dto.assetId
-        }
+	async createAsset(user, dto): Promise<CreateAssetResponse> {
+		try {
+			const watchlist = {
+				user: user.id,
+				name: dto.name,
+				assetId: dto.assetId,
+			}
 
-        await this.watchlistRepository.create(watchlist)
-        return watchlist
-    }
+			await this.watchlistRepository.create(watchlist)
+			return watchlist
+		} catch (err) {
+			throw new Error(err)
+		}
+	}
 
-    async deleteAsset (userId: number, assetId: string): Promise<boolean> {
-        await this.watchlistRepository.destroy({where: {
-            id: assetId,
-            user: userId
-        }})
-        return true
-    }
+	async deleteAsset(userId: number, assetId: string): Promise<boolean> {
+		try {
+			await this.watchlistRepository.destroy({
+				where: {
+					id: assetId,
+					user: userId,
+				},
+			})
+			return true
+		} catch (err) {
+			throw new Error(err)
+		}
+	}
 }
